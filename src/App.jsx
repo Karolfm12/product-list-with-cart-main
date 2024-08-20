8f2414761aca865ec69dbe31d78e99563b42727e
Karol Kotański <karolfm12@onet.eu>
2024-08-20 14:43:04 +0200

    4#commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     try {
       const res = await fetch("../data.json");
       const data = await res.json();
+
       setData(data);
       console.log(data);
     } catch (error) {
@@ -20,8 +21,14 @@ function App() {
     fetchItems();
   }, []);
 
-  const handleButton = () => {
+  const handleButton = (e) => {
     setIsAdded(true);
+
+    const styles = e.currentTarget.style;
+    styles.backgroundColor = "hsl(14, 86%, 42%)";
+    styles.color = "white";
+    styles.display = "flex";
+    styles.justifyContent = "space-between";
     // setIsAdded(true);
     //   const styles = e.currentTarget.style;
     //   styles.width = "70%";
@@ -43,22 +50,56 @@ function App() {
           <ul className="items-list">
             {data.map((item, i) => (
               <li className="item" key={i}>
-                <img src={item.image.desktop} alt="" className="item-image" />
-                <button className="button-add-to-cart" onClick={handleButton}>
+                <img
+                  src={item.image.desktop}
+                  alt=""
+                  className="item-image"
+                />
+                <button
+                  className="button-add-to-cart"
+                  onClick={handleButton}
+                >
                   {isAdded ? (
-                    <></>
+                    <>
+                      <img
+                        src="../assets/images/icon-decrement-quantity.svg"
+                        alt=""
+                        className="incDec"
+                        onClick={() =>
+                          setCount(count - 1)
+                        }
+                      />
+                      {count}
+                      <img
+                        src="../assets/images/icon-increment-quantity.svg"
+                        alt=""
+                        className="incDec"
+                        onClick={() =>
+                          setCount(count + 1)
+                        }
+                      />
+                    </>
                   ) : (
                     <>
-                      <img src="../assets/images/icon-add-to-cart.svg" alt="" />
+                      <img
+                        src="../assets/images/icon-add-to-cart.svg"
+                        alt=""
+                      />
                       Add to Cart
                     </>
                   )}
                 </button>
 
                 <div className="item-details">
-                  <p className="item-category">{item.category}</p>
-                  <p className="item-name">{item.name}</p>
-                  <p className="item-price">${item.price.toFixed(2)}</p>
+                  <p className="item-category">
+                    {item.category}
+                  </p>
+                  <p className="item-name">
+                    {item.name}
+                  </p>
+                  <p className="item-price">
+                    ${item.price.toFixed(2)}
+                  </p>
                 </div>
               </li>
             ))}
