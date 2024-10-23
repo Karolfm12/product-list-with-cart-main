b92c91b6306e02c4a6fb5dd5b08759082fb8ffc5
Karol Kotański <karolfm12@onet.eu>
2024-10-23 11:21:42 +0200

    deploy1try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/product-list-with-cart-main/",
+  // base: "/product-list-with-cart-main/",
 });
